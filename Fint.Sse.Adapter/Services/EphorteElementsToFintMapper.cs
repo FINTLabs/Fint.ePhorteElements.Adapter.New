dc7867ab1ad7d1818f522bd9be83207b04612933
Jørgen Tellnes <jorgen@arkitektum.no>
2019-11-21 21:43:53 +0100

    Add ePhorte Elements to FINT mapper

---
+++ Fint.Sse.Adapter/Services/EphorteElementsToFintMapper.cs
@@ -0,0 +1,9 @@
+using FINT.Model.Administrasjon.Arkiv;
+using Gecko.NCore.Client.ObjectModel.V3.En;
+
+namespace Fint.Sse.Adapter.Services
+{
+    public static class EphorteElementsToFintMapper
+    {
+    }
+}
