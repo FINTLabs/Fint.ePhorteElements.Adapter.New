b316faec8145e62583af935bd72959143e654b58
Jørgen Tellnes <jorgen@arkitektum.no>
2019-12-04 13:14:52 +0100

    Refactor

--- Fint.Sse.Adapter/Mapping/EphorteElementsToFintMapper.cs
+++ Fint.Sse.Adapter/Mapping/EphorteElementsToFintMapper.cs
@@ -13,8 +13,8 @@ namespace Fint.Sse.Adapter.Mapping
         {
             var sak = new SakResource
             {
-                SystemId = new Identifikator { Identifikatorverdi = @case.Id.ToString() },
-                MappeId = new Identifikator { Identifikatorverdi = $"{@case.CaseYear}/{@case.SequenceNumber}" },
+                SystemId = new Identifikator {Identifikatorverdi = @case.Id.ToString()},
+                MappeId = new Identifikator {Identifikatorverdi = $"{@case.CaseYear}/{@case.SequenceNumber}"},
                 Sakssekvensnummer = @case.SequenceNumber.ToString(),
                 Tittel = @case.Title,
                 OffentligTittel = @case.PublicTitle,
@@ -26,17 +26,17 @@ namespace Fint.Sse.Adapter.Mapping
             };
 
             sak.AddSaksstatus(Link.with(typeof(Saksstatus), "systemid", @case.CaseStatusId));
-           
+
             return sak;
         }
 
-        public static List<Saksstatus> MapCaseStatuses(IEnumerable<CaseStatus> caseStatuses)
+        public static IEnumerable<Saksstatus> MapCaseStatuses(IEnumerable<CaseStatus> caseStatuses)
         {
-            var mapCaseStatuses = new List<Saksstatus>();
+            var saksstatuses = new List<Saksstatus>();
 
             foreach (var caseStatus in caseStatuses)
             {
-                mapCaseStatuses.Add(new Saksstatus
+                saksstatuses.Add(new Saksstatus
                 {
                     SystemId = new Identifikator
                     {
@@ -47,9 +47,9 @@ namespace Fint.Sse.Adapter.Mapping
                 });
             }
 
-            return mapCaseStatuses;
+            return saksstatuses;
         }
-        
+
         private static DateTime SetUtcTimeZone(DateTime? dateTime)
         {
             return DateTime.SpecifyKind(dateTime.GetValueOrDefault(), DateTimeKind.Utc);
