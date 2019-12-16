a3755c36e98a6e64602440edfcd105726b111589
Jørgen Tellnes <jorgen@arkitektum.no>
2019-12-16 15:15:41 +0100

    Cleanup request class

--- Fint.Sse.Adapter/FintRequest.cs
+++ Fint.Sse.Adapter/FintRequest.cs
@@ -1,21 +1,18 @@
-using System;
 using System.Collections.Generic;
-using FINT.Model.Administrasjon.Arkiv;
 
 namespace Fint.Sse.Adapter
 {
     public class FintRequest
     {
-        public string Action{ get; }
+        public string Action { get; }
         public FintQuery Query { get; }
         public IEnumerable<object> Data { get; }
 
         public FintRequest(string action, string query = null, IEnumerable<object> data = null)
         {
-            //Action = (Enum) Enum.Parse(typeof(Enum), action, ignoreCase: true);
             Action = action;
 
-            if(query != null)
+            if (query != null)
                 Query = new FintQuery(query);
 
             Data = data;
